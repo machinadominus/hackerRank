15baee0ef0bc36742431c8e16ae3d1bd3f85c577
sohail <mohammedsohail98201@gmail.com>
2024-02-04 17:24:15 +0530

    Update Basic Data Types.cpp

--- cpp/Basic Data Types.cpp
+++ cpp/Basic Data Types.cpp
@@ -1,1 +1,19 @@
-h
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long long b;
+    char ch;
+    float f;
+    double d;
+    cin >> a >> b >> ch >> f >> d;
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%c\n", ch);
+    printf("%f\n", f);
+    printf("%lf\n", d);
+    return 0;
+}
